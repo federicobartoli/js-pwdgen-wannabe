c14e317dc2b4e7a42848d8493311066e47cf6476
Federico <federicobartoli@me.com>
2020-02-17 14:23:04 +0100

    Esercizio finito , ora incomincio a prendere il 20 dall'anno

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 var nome = prompt('Come ti chiami?');
 var cognome = prompt('E il tuo cognome?');
 var colore = prompt('Colore preferito?');
+
+document.getElementById('passwordgenerator').innerHTML = nome + cognome + colore + '20';
