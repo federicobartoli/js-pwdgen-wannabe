1bd7b8ba7a03231030b43b3a3dc8b09a2db263ad
Federico <federicobartoli@me.com>
2020-02-17 14:34:02 +0100

    Aggiunto esercizio facoltativo1 in index2

---
+++ js2/main.js
@@ -0,0 +1,6 @@
+var nome = prompt('Scrivi il tuo nome');
+var cognome = prompt('Ora il tuo cognome');
+var colore = prompt('Ora il tuo colore preferito');
+var data = new Date();
+var anno = data.getFullYear();
+document.getElementById('passwordgenerator').innerHTML = nome + cognome + colore + anno;
