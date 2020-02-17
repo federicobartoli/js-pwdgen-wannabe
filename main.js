3481281836e614aa270c44b6df4c32b9dadbd2c3
Federico <federicobartoli@me.com>
2020-02-17 14:28:30 +0100

    Esercizio finito

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var nome = prompt('Come ti chiami?');
-var cognome = prompt('E il tuo cognome?');
-var colore = prompt('Colore preferito?');
+var nome = prompt('Scrivi il tuo nome');
+var cognome = prompt('Ora il tuo cognome');
+var colore = prompt('Ora il tuo colore preferito');
 
 document.getElementById('passwordgenerator').innerHTML = nome + cognome + colore + '20';
